64aa5df34faf5b8fbe32f039105789cd7c45b539
Hannah Hudson <hhudson@riversagency.com>
2018-10-01 21:04:22 -0400

    rm react router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Switch>
-        <Route exact path = "/" component={App} />
-      </Switch>
-  </BrowserRouter>
-
+    <App/>
 
     ,document.getElementById('root')
 );
